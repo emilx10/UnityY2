c95c32e397c690794ebb959708a5657b63d56f46
Ron1405 <ronsade159874@gmail.com>
2024-12-21 22:32:28 +0200

    Variation area
    Added variation area with a dynamic cost

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,13 +1,20 @@
+using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Movement : MonoBehaviour
 {
     public Transform EndGoal;
+    public NavMeshSurface surface;
+    public int AreaCost;
     void Start()
     {
+
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.SetAreaCost(NavMesh.GetAreaFromName("SkinnyObstacle"), AreaCost);
         agent.destination = EndGoal.position;
+
+        
     }
 
     
