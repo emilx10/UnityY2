bf389c8b66c690a509e90ff09f07051117209513
emilx10 <51999563+emilx10@users.noreply.github.com>
2024-12-23 08:08:23 +0200

    Finishing touches
    added a pop up text

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,25 +1,33 @@
 using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Movement : MonoBehaviour
 {
     public Transform EndGoal;
     public NavMeshSurface surface;
     public int AreaCost;
+    public NavMeshAgent agent;
+    public Text textPopup;
     void Start()
     {
-
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
         agent.SetAreaCost(NavMesh.GetAreaFromName("SkinnyObstacle"), AreaCost);
         agent.destination = EndGoal.position;
-
+        textPopup.gameObject.SetActive(false);
         
     }
+    private void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            textPopup.gameObject.SetActive(true);
+        }
+    }
 
-    
-    
-    
-        
-    
+
+
+
+
+
 }
