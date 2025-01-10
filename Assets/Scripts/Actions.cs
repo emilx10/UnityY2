9c051fb550e1567afef7baa4a12641155c22c7d6
emilx10 <emilxt12@gmail.com>
2025-01-10 23:38:52 +0200

    ScriptStuff

--- Assets/Scripts/Actions.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System;
-using UnityEngine.Events;
-
-public class UnityActions: MonoBehaviour
-{
-    public static Action coinPickUp;
-    public static UnityAction<PlayerHealthManager> PlayerHealth;
-}
--- Assets/Scripts/PlayerHealthManager.cs
+++ Assets/Scripts/PlayerHealthManager.cs
@@ -1,6 +1,22 @@
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class PlayerHealthManager : MonoBehaviour
 {
-    
+    public Text textHealth;
+    public int health = 100;
+
+    private void Start()
+    {
+        textHealth.text = health.ToString();
+    }
+
+    public void TakeDmg()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
 }
