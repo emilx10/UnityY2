473d2af6a2b76ca50daa164c51a2ae756cdaefb0
emilx10 <emilxt12@gmail.com>
2025-02-01 12:51:20 +0200

    Animator
    Added animator for player 1 with idle and walking animations based on speed float condition

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,24 +10,46 @@ public class Movement : MonoBehaviour
     public int AreaCost;
     public NavMeshAgent agent;
     public Text textPopup;
+    public Animator animator;
+
+    private float idleTimer = 0f;
+    private float idleDuration = 2f; 
+    private bool canMove = false;
+
     void Start()
     {
         agent.SetAreaCost(NavMesh.GetAreaFromName("SkinnyObstacle"), AreaCost);
-        agent.destination = EndGoal.position;
         textPopup.gameObject.SetActive(false);
-        
+        animator = GetComponent<Animator>();
+
+        agent.isStopped = true;
+        animator.SetFloat("Speed", 0);
     }
+
     private void Update()
     {
+        if (!canMove)
+        {
+            idleTimer += Time.deltaTime;
+
+            if (idleTimer >= idleDuration)
+            {
+                canMove = true;
+                agent.isStopped = false;
+                agent.destination = EndGoal.position;
+            }
+        }
+
+        float speed = agent.velocity.magnitude;
+
+        if (canMove)
+        {
+            animator.SetFloat("Speed", speed);
+        }
+
         if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
         {
             textPopup.gameObject.SetActive(true);
         }
     }
-
-
-
-
-
-
 }
