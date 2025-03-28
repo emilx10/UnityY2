cc9299f4826093298fae6efa515c55aa5d1b1c5e
Ron1405 <ronsade159874@gmail.com>
2025-03-28 19:10:25 +0300

    Map
    Changes to barrel behavior and added a level

--- Assets/Scripts/ObstaclesMovement.cs
+++ Assets/Scripts/ObstaclesMovement.cs
@@ -1,31 +1,37 @@
 using System;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class ObstaclesMovement : MonoBehaviour
 {
-    [SerializeField] private GameObject[] obstaclesPoints;
-    [SerializeField] private BarrelScriptableObject barrelStats;
+    [SerializeField] private GameObject[] obstaclesPoints; 
+    [SerializeField] private BarrelScriptableObject barrelStats; 
+    [SerializeField] private float speed = 5f; 
 
-    private bool isDestPoint1 = true;
-    
+    private bool isDestPoint1 = true; // Flag to check if the object is moving towards point 1 or point 2
+
     void Update()
     {
+        // If the object is moving towards the first point
         if (isDestPoint1)
         {
+            // If the object reaches the first point, change the destination to the second point
             if (Vector3.Distance(transform.position, obstaclesPoints[0].transform.position) < 1f)
                 isDestPoint1 = false;
             else
+                // Move the object towards the first point
                 transform.position = new Vector3(
-                    transform.position.x - (barrelStats.BarrelSpeedMod * Time.deltaTime), transform.position.y, transform.position.z);
+                    transform.position.x - (barrelStats.BarrelSpeedMod * speed * Time.deltaTime), transform.position.y, transform.position.z);
         }
         else
         {
-            if (Vector3.Distance(transform.position, obstaclesPoints[1].transform.position) < 1f)
-                isDestPoint1 = true;
-            else
-                transform.position = new Vector3(
-                    transform.position.x + (barrelStats.BarrelSpeedMod * Time.deltaTime), transform.position.y, transform.position.z);
+            // If the object reaches the second point, destroy it
+            if (Vector3.Distance(transform.position, obstaclesPoints[0].transform.position) < 1f)
+            {
+                Destroy(gameObject); 
+                return; // Exit the update loop to stop any further movement after destruction
+            }
+
+           
         }
     }
 }
